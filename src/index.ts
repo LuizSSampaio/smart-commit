c0bf3d77661750eb790da2d2e2916df97da99534
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-11-21 20:37:13 -0300

    feat(command): add draft subcommand

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { command, option, optional, run, string } from "cmd-ts"
+import { command, option, optional, run, string, subcommands } from "cmd-ts"
 
-const cmd = command({
-  name: 'smart-commit',
-  version: '0.1.0',
+const draft = command({
+  name: 'draft',
   args: {
+    context: option({
+      long: 'context',
+      short: 'c',
+      type: optional(string)
+    }),
+    type: option({
+      long: 'type',
+      short: 't',
+      type: optional(string)
+    }),
     scope: option({
       long: 'scope',
       short: 's',
@@ -13,6 +22,13 @@ const cmd = command({
   handler: (args) => {
     console.log(args)
   }
+})
+
+const cmd = subcommands({
+  name: 'smart-commit',
+  version: '0.1.0',
+  description: 'TODO',
+  cmds: { draft }
 })
 
 run(cmd, process.argv.slice(2))
