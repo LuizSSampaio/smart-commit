5cbb393b1926c5671873d6022fbd9d27a3f6de83
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-11-24 13:39:23 -0300

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LuizSSampaio/smart-commit
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/LuizSSampaio/smart-commit/internal/cmd"
+
+func main() {
+	cmd.Execute()
+}
