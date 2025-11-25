ef6210f34f772794c0d597d43e99d3a04a28ef5c
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-11-25 16:17:00 -0300

    refactor: remove cmd from internal

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/LuizSSampaio/smart-commit/internal/cmd"
+import "github.com/LuizSSampaio/smart-commit/cmd"
 
 func main() {
 	cmd.Execute()
